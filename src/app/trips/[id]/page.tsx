22bb5d91b2b07c7d52a6193264fba6a287e665c2
Jefferson <jefferson.brandao@g.globo>
2023-07-05 20:59:23 -0300

    feat: create trip description component

--- src/app/trips/[id]/page.tsx
+++ src/app/trips/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma";
-import React from "react";
 import TripHeader from "./components/TripHeader";
 import TripReservation from "./components/TripReservation";
+import TripDescription from "./components/TripDescription";
 
 const getTripDetails = async (id: string) => {
   const trip = await prisma.trip.findUnique({
@@ -24,6 +24,7 @@ const TripsDetails = async ({ params }: { params: { id: string } }) => {
     <div className="container mx-auto">
       <TripHeader trip={trip} />
       <TripReservation trip={trip} />
+      <TripDescription description={trip.description} />
     </div>
   );
 };
